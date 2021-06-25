69445ccf7e947cbabf104661af20801d0199ee0d
fastcampus-kyle <java_kwon@fcacademy.co.kr>
2021-06-25 22:38:03 +0900

    Chapter 03. JPA - 02. Lombok (1)

--- src/main/java/com/fastcampus/jaavallinone/project3/mycontact/domain/Person.java
+++ src/main/java/com/fastcampus/jaavallinone/project3/mycontact/domain/Person.java
@@ -1,11 +1,17 @@
 package com.fastcampus.jaavallinone.project3.mycontact.domain;
 
+import java.time.LocalDate;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
+@Getter
+@Setter
+@ToString
 public class Person {
 
   @Id
@@ -16,36 +22,16 @@ public class Person {
 
   private int age;
 
-  public Long getId() {
-    return id;
-  }
+  private String hobby;
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+  private String bloodType;
 
-  public String getName() {
-    return name;
-  }
+  private String address;
 
-  public void setName(String name) {
-    this.name = name;
-  }
+  private LocalDate birthday;
 
-  public int getAge() {
-    return age;
-  }
+  private String job;
 
-  public void setAge(int age) {
-    this.age = age;
-  }
-
-  @Override
-  public String toString() {
-    return "Person{" +
-        "id=" + id +
-        ", name='" + name + '\'' +
-        ", age=" + age +
-        '}';
-  }
+  @ToString.Exclude
+  private String phoneNumber;
 }
