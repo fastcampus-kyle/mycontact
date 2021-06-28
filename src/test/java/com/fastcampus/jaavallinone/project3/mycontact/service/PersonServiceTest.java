3ecde5e48b45807ef92e6e57e562add7398269d9
fastcampus-kyle <java_kwon@fcacademy.co.kr>
2021-06-28 17:43:37 +0900

    Chapter 08. Mock-Test - 02. Service Test #2

--- src/test/java/com/fastcampus/jaavallinone/project3/mycontact/service/PersonServiceTest.java
+++ src/test/java/com/fastcampus/jaavallinone/project3/mycontact/service/PersonServiceTest.java
@@ -1,11 +1,17 @@
 package com.fastcampus.jaavallinone.project3.mycontact.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.fastcampus.jaavallinone.project3.mycontact.controller.dto.PersonDto;
 import com.fastcampus.jaavallinone.project3.mycontact.domain.Person;
 import com.fastcampus.jaavallinone.project3.mycontact.repository.PersonRepository;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,7 +31,7 @@ class PersonServiceTest {
   private PersonRepository personRepository;
 
   @Test
-  void getPepopleByName(){
+  void getPepopleByName() {
     when(personRepository.findByName("martin"))
         .thenReturn(Lists.newArrayList(new Person("martin")));
 
@@ -33,5 +39,35 @@ class PersonServiceTest {
 
     assertThat(result.size()).isEqualTo(1);
     assertThat(result.get(0).getName()).isEqualTo("martin");
+  }
+
+  @Test
+  void getPerson() {
+    when(personRepository.findById(1L))
+        .thenReturn(Optional.of(new Person("martin")));
+
+    Person person = personService.getPerson(1L);
+
+    assertThat(person.getName()).isEqualTo("martin");
+  }
+
+  @Test
+  void getPersonifNotFount() {
+    when(personRepository.findById(1L))
+        .thenReturn(Optional.empty());
+
+    Person person = personService.getPerson(1L);
+
+    assertThat(person).isNull();
+  }
+
+  @Test
+  void put() {
+    PersonDto dto = PersonDto
+        .of("martin", "programming", "판교", LocalDate.now(), "programmer", "010-1111-2222");
+
+    personService.put(dto);
+
+    verify(personRepository, times(1)).save(any(Person.class));
   }
 }
